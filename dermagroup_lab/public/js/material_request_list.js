7e4354ffb772c22ec687f1dd2dc241523add2d15
Pedro Iván Fernández González <pedroifgonzalez@gmail.com>
2025-12-29 12:53:20 -0500

    [REF] Keep consitency in statuses colors

--- dermagroup_lab/public/js/material_request_list.js
+++ dermagroup_lab/public/js/material_request_list.js
@@ -1,21 +1,22 @@
+const STATUS_CONFIG = {
+	"Pending Approval": "blue",
+	"Under Review": "orange",
+	Approved: "green",
+	"Sent to Supplier": "green",
+	Confirmed: "blue",
+	"Pending Delivery": "yellow",
+	Cancelled: "red",
+};
+
 frappe.listview_settings["Material Request"] = {
 	has_indicator_for_draft: true,
 	has_indicator_for_cancelled: true,
 	get_indicator: function (doc) {
-		if (doc.status === "Pending Approval") {
-			return [__("Pending Approval"), "blue", "status,=,Pending Approval"];
-		} else if (doc.status === "Under Review") {
-			return [__("Under Review"), "orange", "status,=,Under Review"];
-		} else if (doc.status === "Approved") {
-			return [__("Approved"), "green", "status,=,Approved"];
-		} else if (doc.status === "Sent to Supplier") {
-			return [__("Sent to Supplier"), "green", "status,=,Sent to Supplier"];
-		} else if (doc.status === "Confirmed") {
-			return [__("Confirmed"), "blue", "status,=,Confirmed"];
-		} else if (doc.status === "Pending Delivery") {
-			return [__("Pending Delivery"), "yellow", "status,=,Pending Delivery"];
-		} else if (doc.status === "Cancelled") {
-			return [__("Cancelled"), "red", "status,=,Cancelled"];
+		const color = STATUS_CONFIG[doc.status];
+		if (color) {
+			return [__(doc.status), color, `status,=,${doc.status}`];
+		} else {
+			return [__(doc.status), "grey", `status,=,${doc.status}`];
 		}
 	},
 };
